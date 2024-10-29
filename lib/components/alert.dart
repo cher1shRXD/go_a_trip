3c21e45f82e22f37a2c89c6f135e94cadd40ab38
cher1shRXD <taewoo080401@gmail.com>
2024-10-29 22:04:11 +0900

    feat::button go down

--- lib/components/alert.dart
+++ lib/components/alert.dart
@@ -7,8 +7,12 @@ void alert(BuildContext context, String message, String type) async {
     barrierDismissible: true,
     builder: (BuildContext context) {
       return AlertDialog(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16),
+        ),
         actionsAlignment: MainAxisAlignment.center,
         actionsPadding: EdgeInsets.zero,
+        backgroundColor: const Color.fromARGB(230, 230, 230, 230),
         content: SingleChildScrollView(
           child: ListBody(children: [
             Row(children: [
@@ -29,7 +33,9 @@ void alert(BuildContext context, String message, String type) async {
                 ),
               ),
             ]),
-            Text(message, style: const TextStyle(fontSize: 16)),
+            Text(message,
+                style:
+                    const TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
           ]),
         ),
         actions: [
@@ -41,15 +47,16 @@ void alert(BuildContext context, String message, String type) async {
                 width: double.infinity,
                 decoration: BoxDecoration(
                   border: Border(
-                    top: BorderSide(color: Colors.grey[300]!, width: 1),
+                    top: BorderSide(color: Colors.grey[500]!, width: 0.3),
                   ),
                 ),
                 padding: const EdgeInsets.only(
                     left: 16, right: 16, top: 12, bottom: 12),
                 child: const Text(
                   '닫기',
-                  style: TextStyle(color: Colors.black),
-                  textAlign: TextAlign.center, // 텍스트 중앙 정렬
+                  style: TextStyle(
+                      color: Color.fromARGB(255, 0, 0, 255), fontSize: 16),
+                  textAlign: TextAlign.center,
                 ),
               ),
               onPressed: () {
